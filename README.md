b60540b1a1e4c8a0ff5f3ce52237f2fa217f9cb6
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-03-26 23:47:53 -0500

    agregando seccion de funcionalidad al readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# beneficencia
+## funcionalidad
---
+++ js/java-script-validacion.js
@@ -0,0 +1,10 @@
+
+function validar()
+{
+if (document.miFormulario.txtAreaConsulta.value.length==0 ) {
+alert("textarea vacio");
+return false;
+}
+else return true;
+
+}
